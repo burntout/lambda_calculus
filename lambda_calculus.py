09edfb6fee96f7b2faacd5e579cc613f92278390
Kate Dawson <k4t@3msg.es>
2017-09-26 16:52:06 +0100

    Refactor

--- lambda_calculus.py
+++ lambda_calculus.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-from helpers import *
-
 '''
 Another go  with Lambda calculus
 This time though with changes to Booleans, wrapping them in additional lambda
@@ -20,28 +18,6 @@ OR = lambda x: lambda y: x(lambda _: TRUE)(lambda _: y(lambda _: TRUE)(lambda _:
 AND = lambda x: lambda y: x(lambda _: y(lambda _: TRUE)(lambda _: FALSE))(lambda _: FALSE)
 
 IF = lambda c: lambda t: lambda e: c(t)(e)
-
-'''
-Boolean Tests
-'''
-
-assert TRUE(lambda _: True)(lambda _: False) == True
-assert FALSE(lambda _: True)(lambda _: False) == False
-assert NOT(TRUE)(lambda _: True)(lambda _: False) == False
-assert NOT(FALSE)(lambda _: True)(lambda _: False) == True
-
-assert OR(TRUE)(TRUE)(lambda _: True)(lambda _: False) == True
-assert OR(TRUE)(FALSE)(lambda _: True)(lambda _: False) == True
-assert OR(FALSE)(TRUE)(lambda _: True)(lambda _: False) == True
-assert OR(FALSE)(FALSE)(lambda _: True)(lambda _: False) == False
-
-assert AND(TRUE)(TRUE)(lambda _: True)(lambda _: False) == True
-assert AND(TRUE)(FALSE)(lambda _: True)(lambda _: False) == False
-assert AND(FALSE)(TRUE)(lambda _: True)(lambda _: False) == False
-assert AND(FALSE)(FALSE)(lambda _: True)(lambda _: False) == False
-
-assert IF(TRUE)(lambda _: "Yes")(lambda _: "No") == "Yes"
-assert IF(FALSE)(lambda _: "Yes")(lambda _: "No") == "No"
 
 ''' 
 Natural Numbers
@@ -84,29 +60,6 @@ PRED = lambda n: n(PSUCC)(ZZ)(FST)
 MINUS = lambda m: lambda n: n(PRED)(m)
 
 '''
-Integer Tests
-'''
-
-assert ZERO(lambda x: x+1)(0) == 0
-assert ONE(lambda x: x+1)(0) == 1
-assert TWO(lambda x: x+1)(0) == 2
-
-assert ADD(ONE)(ONE)(lambda x: x+1)(0) == 2
-assert ADD(ONE)(TWO)(lambda x: x+1)(0) == 3
-assert ADD(TWO)(ONE)(lambda x: x+1)(0) == 3
-assert ADD(TWO)(TWO)(lambda x: x+1)(0) == 4
-
-assert MULT(THREE)(TWO)(lambda x: x+1)(0) == 6
-assert MULT(TWO)(THREE)(lambda x: x+1)(0) == 6
-
-assert PRED(THREE)(lambda x: x+1)(0) == 2
-assert PRED(FOUR)(lambda x: x+1)(0) == 3
-
-assert MINUS(FOUR)(ONE)(lambda x: x+1)(0) == 3 
-assert MINUS(FOUR)(THREE)(lambda x: x+1)(0) == 1
-
-
-'''
 Comparisons
 '''
 
@@ -116,30 +69,6 @@ GT = lambda m: lambda n: AND(IS_ZERO(MINUS(m)(n)))(NOT(IS_ZERO(MINUS(n)(m))))
 LT = lambda m: lambda n: AND(NOT(IS_ZERO(MINUS(m)(n))))(IS_ZERO(MINUS(n)(m))) 
 GE = lambda m: lambda n: NOT(LT(m)(n))
 
-'''
-Comparison Tests
-'''
-
-assert IS_ZERO(ZERO)(lambda _: True)(lambda _: False) == True
-assert IS_ZERO(ONE)(lambda _: True)(lambda _: False) == False
-assert IS_ZERO(TWO)(lambda _: True)(lambda _: False) == False
-assert IF(IS_ZERO(ZERO))(lambda _: "Yes")(lambda _: "No") == "Yes"
-assert IF(IS_ZERO(ONE))(lambda _: "Yes")(lambda _: "No") == "No"
-
-assert EQ(ONE)(ONE)(lambda _: True)(lambda _: False) == True
-assert EQ(SUCC(SUCC(SUCC(SUCC(ZERO)))))(ADD(TWO)(TWO))(lambda _: True)(lambda _: False) == True
-
-assert LT(FOUR)(THREE)(lambda _: True)(lambda _: False) == True
-assert LT(THREE)(FOUR)(lambda _: True)(lambda _: False) == False
-assert LT(THREE)(THREE)(lambda _: True)(lambda _: False) == False
-
-assert GT(FOUR)(THREE)(lambda _: True)(lambda _: False) == False
-assert GT(THREE)(FOUR)(lambda _: True)(lambda _: False) == True
-assert GT(THREE)(THREE)(lambda _: True)(lambda _: False) == False
-
-assert GE(THREE)(THREE)(lambda _: True)(lambda _: False) == True
-assert GE(THREE)(TWO)(lambda _: True)(lambda _: False) == False
-assert GE(THREE)(FOUR)(lambda _: True)(lambda _: False) == True
 
 '''
 Recursive Functions
@@ -175,14 +104,6 @@ RMULT = lambda a: lambda b: Z(m)(a)(b)
 # Euclids gcd algorithm
 GCD_STUB = lambda f: lambda a: lambda b: IF(IS_ZERO(b))(lambda _: a)(lambda _: f(b)(REMAINDER(a)(b)))
 GCD = lambda a: lambda b: Z(GCD_STUB)(a)(b)
-
-
-assert FACTORIAL(SUCC(FOUR))(lambda x: x+1)(0) == 120
-assert FIBONACCI(FOUR)(lambda x: x+1)(0) == 5
-assert DIVIDE(MULT(FOUR)(TWO))(THREE)(lambda x: x+1)(0) == 2
-assert REMAINDER(MULT(FOUR)(TWO))(THREE)(lambda x: x+1)(0) == 2
-assert REMAINDER(TEN)(FOUR)(lambda x: x+1)(0) == 2
-
 
 '''
 Lists
@@ -224,15 +145,3 @@ FILTER_STUB = lambda f: lambda p: lambda l: IF(IS_NIL(l))(lambda _: l)(lambda _:
 FILTER = lambda p: lambda l: Z(FILTER_STUB)(p)(l)
 
 
-l = CONS(ONE)(CONS(TWO)(CONS(THREE)(CONS(FOUR)(NIL))))
-k = MAP(MULT(TEN))(l)
-
-assert HEAD(l)(lambda x: x+1)(0) == 1
-assert HEAD(TAIL(l))(lambda x: x+1)(0) == 2
-assert HEAD(TAIL(TAIL(k)))(lambda x: x+1)(0) == 30
-assert HEAD(TAIL(TAIL(TAIL(k))))(lambda x: x+1)(0) == 40
-assert LEN(l)(lambda x: x+1)(0) == 4
-assert LEN(APPEND(l)(FIVE))(lambda x: x+1)(0) == 5
-assert LAST(APPEND(l)(FIVE))(lambda x: x+1)(0) == 5
-assert HEAD(REVERSE(l))(lambda x: x+1)(0) == 4
-assert churchlist_to_list(FILTER(GT(TWO))(l)) == [3, 4]
